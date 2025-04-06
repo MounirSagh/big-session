3bfea64bac5a4062893a99d182482ed5e5c53824
Mohamed Fadli <mohamedfadli2004@gmail.com>
2025-04-06 19:08:42 +0100

    Simple frontend example

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,24 @@
 import { useState } from 'react'
+import BugCard from './bugCard';
+import { Box, Divider, Typography, Button } from '@mui/material';
+import Add from '../components/add'
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Box padding="32px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Typography variant='h3' marginBottom="16px">
+          List of Current Bugs
+        </Typography>
+        <Button variant='contained' style={{maxHeight: '64px', borderRadius:'0.8em'}}>
+          Add Bug
+        </Button>
+      </Box>
+      <Divider />
+      <Box marginTop="16px">
+        <BugCard BugTitle="Sample Title" BugDescription="Sample Description" />
+      </Box>
+    </Box>
   )
 }
 
